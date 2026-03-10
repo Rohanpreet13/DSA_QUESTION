e2eab74b8c356ce10473ff066c2aaf36b240971e
Rohanpreet13 <rohanpreetarora1311@gmail.com>
2026-03-10 17:51:05 +0530

    Implement binary search in Problem_09.c

---
+++ Problem_09.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int binarySearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+int main() {
+    int n, target;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter sorted array elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter target value: ");
+    scanf("%d", &target);
+    int result = binarySearch(arr, n, target);
+    if(result != -1)
+        printf("%d", result);
+    else
+        printf("Element not found");
+    return 0;
+}
